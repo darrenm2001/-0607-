4aecee7add58ef7e3f3dc71f85a67d5167bf2164
darren moo <tsc97163@gmail.com>
2023-06-07 19:09:13 +0800

    第三题

---
+++ 期末exam/Controllers/TestController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace 期末exam.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Eaxm()
+        {
+            return View();
+        }
+    }
+}
