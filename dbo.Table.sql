3a414c8ae947799dcfb6e1dfe79b81c4718e1d87
darren moo <tsc97163@gmail.com>
2023-06-07 20:14:09 +0800

    期末

---
+++ dbo.Table.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Title] NCHAR(50) NOT NULL, 
+    [Content] NCHAR(500) NOT NULL, 
+    [PublishData] DATETIME2 NOT NULL
+)
---
+++ 期末exam/Controllers/UserController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace 期末exam.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index(string name,float? num)
+        {
+            var result = "";
+            if (num is 1 )
+            {
+                result ="do";
+            }
+            else if (num is 2)
+            {
+                result = "re";
+            }
+            else if (num is 3)
+            {
+                result = "mi";
+            }
+            else if (num is 4)
+            {
+                result = "fa";
+            }
+            else if (num is 5)
+            {
+                result = "so";
+            }
+            else if (num is 6)
+            {
+                result = "la";
+            }
+            if (num is 7)
+            {
+                result = "xi";
+            }
+
+
+            ViewBag.Name = name;    
+                ViewBag.Num = num;  
+
+
+            return View();
+        }
+        public ActionResult Music()
+        {
+            return View();
+        }
+
+    }
+}
